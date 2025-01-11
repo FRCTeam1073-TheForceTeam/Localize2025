53c580891d75240a1338972c655a60c07ba1d9cd
L-i-y-a <92765324+Mindi088@users.noreply.github.com>
2025-01-11 14:06:05 -0500

    did things
    added things

--- src/main/java/frc/robot/subsystems/AprilTagFinder.java
+++ src/main/java/frc/robot/subsystems/AprilTagFinder.java
@@ -10,16 +10,49 @@ import java.util.List;
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonUtils;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class AprilTagFinder extends SubsystemBase {
+public class AprilTagFinder extends SubsystemBase 
+{
+  public class VisionMeasurement
+  {
+    public Pose2d pose;
+    public double timeStamp;
+    public int tagID;
+
+    public VisionMeasurement(Pose2d pose, double timeStamp, int tagID)
+    {
+      this.pose = pose;
+      this.timeStamp = timeStamp;
+      this.tagID = tagID;
+    }
+  }
+
   public PhotonCamera frontLeftCam = new PhotonCamera("LeftCamera");
   public PhotonCamera frontRightCam = new PhotonCamera("Arducam_OV9281_USB_Camera");
+  public final Transform3d fLCamTransform3d = new Transform3d(new Translation3d(), new Rotation3d());
+  public final Transform3d fRCamTransform3d = new Transform3d(new Translation3d(), new Rotation3d());
+  public final Transform3d bLCamTransform3d = new Transform3d(new Translation3d(), new Rotation3d());
+  public final Transform3d bRCamTransform3d = new Transform3d(new Translation3d(), new Rotation3d());
   List<PhotonTrackedTarget> responseFL;
   List<PhotonTrackedTarget> responseFR;
+
+  double responseFLTimestamp;
+  double responseFRTimestamp;
+
+  public static final AprilTagFieldLayout map = AprilTagFields.loadField(AprilTagFields.k2025Reefscape);
 
   public int wait_counter = 0;
 
@@ -30,14 +63,44 @@ public class AprilTagFinder extends SubsystemBase {
     return result;
   }
 
-  public void readTagData() {
+  public void readTagData() 
+  {
     responseFL = frontLeftCam.getLatestResult().getTargets();
     responseFR = frontRightCam.getLatestResult().getTargets();
+    responseFLTimestamp = Timer.getFPGATimestamp() - frontLeftCam.getLatestResult().metadata.getLatencyMillis() / 1000.0;
+    responseFRTimestamp = Timer.getFPGATimestamp() - frontRightCam.getLatestResult().metadata.getLatencyMillis() / 1000.0;
+  }
+
+  public ArrayList<VisionMeasurement> getMeasurements()
+  {
+    ArrayList<VisionMeasurement> measurements = new ArrayList<VisionMeasurement>();
+
+    // front left camera
+    PhotonTrackedTarget targetFL = frontLeftCam.getLatestResult().getBestTarget();
+    if (map.getTagPose(targetFL.getFiducialId()).isPresent())
+    {
+      Pose3d robotPoseFL = PhotonUtils.estimateFieldToRobotAprilTag(targetFL.getBestCameraToTarget(),
+                                                                      map.getTagPose(targetFL.getFiducialId()).get(), 
+                                                                      fLCamTransform3d);
+      measurements.add(new VisionMeasurement(robotPoseFL.toPose2d(), responseFLTimestamp, targetFL.getFiducialId()));
+    }
+    
+    // front right camera
+    PhotonTrackedTarget targetFR = frontRightCam.getLatestResult().getBestTarget();
+    if (map.getTagPose(targetFR.getFiducialId()).isPresent())
+    {
+      Pose3d robotPoseFR = PhotonUtils.estimateFieldToRobotAprilTag(targetFR.getBestCameraToTarget(),
+                                                                      map.getTagPose(targetFR.getFiducialId()).get(), 
+                                                                      fRCamTransform3d);
+      measurements.add(new VisionMeasurement(robotPoseFR.toPose2d(), responseFRTimestamp, targetFR.getFiducialId()));
+    }
+
+    return measurements;
   }
 
   @Override
   public void periodic() { 
-    readTagData();
+    readTagData();  
 
     if(responseFL.size() > 0) {
       SmartDashboard.putNumber("FL ID", responseFL.get(0).getFiducialId());
