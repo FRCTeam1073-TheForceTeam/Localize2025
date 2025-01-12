7362165da6e3b16bdc14ee1619e67b4885b0b3eb
AbbyW1212 <113068753+AbbyW1212@users.noreply.github.com>
2025-01-12 09:50:10 -0500

    Started to create scan class, added some methods to lidar

--- src/main/java/frc/robot/subsystems/Lidar.java
+++ src/main/java/frc/robot/subsystems/Lidar.java
@@ -1,4 +1,5 @@
 package frc.robot.subsystems;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
@@ -8,18 +9,24 @@ import edu.wpi.first.wpilibj.Timer;
 public class Lidar extends DiagnosticsSubsystem {
     SerialPort serialPort = new SerialPort(1000000, SerialPort.Port.kUSB1, 8, SerialPort.Parity.kNone, SerialPort.StopBits.kOne);
     byte startCommand[] = new byte[2];
+    boolean measureMode = false;
+    boolean arrayMode = false;
+    //TODO: add scan descriptor to check descriptor string
+    byte scanDescriptor[] = {5A, A5, 05, 00, 00, 40, 81};
     //private final double elevationFactor = 0.94293; //0.97237; //compensating for the fact that it's not level: cos(angle of rangeFinder)
 
     double range = 0.0;
     double filtered_range = 0.0;
     double intensity = 0.0; // starts to die at under 0.005
     double timestamp = 0.0;
+    ArrayList one;
+    ArrayList two;
 
     public Lidar () {
         super.setSubsystem("Lidar");
         serialPort.setFlowControl(SerialPort.FlowControl.kNone);
         
-        startCommand[0] = 0xA5;
+        startCommand[0] = (byte) 0xa5;
         startCommand[1] = 0x20;
 
         Handshake();
@@ -27,7 +34,7 @@ public class Lidar extends DiagnosticsSubsystem {
 
     public void Handshake () {
         byte StopCommand[] = new byte[2];
-        StopCommand[0] = 0xA5;
+        StopCommand[0] = (byte) 0xa5;
         StopCommand[1] = 0x25;
 
         // TODO: fill out stop command
@@ -62,47 +69,80 @@ public class Lidar extends DiagnosticsSubsystem {
     // public double getIntensity() {
     //     return intensity;
     // }
+    public ArrayList getArray(){
+        if(arrayMode){
+            return two;
+        }
+
+        if(!arrayMode){
+            return one;
+        }
+    }
 
     public double getTimestamp() {
         return timestamp;
     }
 
     public void read(){
-        //clockwise is positive - opposite for robot?
+        //clockwise is positive - opposite for robot - convert to radians
         //angle = Math.pow(2, 7) * angle[14:7] + angle[6:0]
+        //distance = Math.pow(2, 8) * distance[15:8] + distance[7:0]
+    }
 
+    public boolean parseDescriptor(){
+        return serialPort.read(7) == scanDescriptor;
+    }
+
+    public void parseMeasurements(){
+        //put data into scan class - return array list of scan object to add to arrayList?
     }
 
     @Override
     public void periodic() {
-        // wait until descriptor is available
+        int bytesToRead = serialPort.getBytesReceived();
+ 
+        if(measureMode){
+            //parse()
+            //fill array based on boolean
+            if(arrayMode){
+                //fill Array one
+            }
+
+            if(!arrayMode){
+                //fill Array two
+            }
+        }
+
+        if(bytesToRead >= 7){
+            // read exactly that many bytes
+            //parseDescriptor();
+            parseDescriptor();
+            measureMode = true;
+        }
+        
+        // TODO create scan class
+        // TODO transform 3D
         // loop until full scan data has been processed
         //      poll until bytes available >= one entry
         //         read one entry
         //read()
         //      parse one entry
         //parse()
-        // if full scan has been complete (S = 1)
-        //    Stop/Restart Scan (Handshake)
-        //      stop scan
-        //      send get_healty request, verify not in Protection Stop (HW Failure) state
-        //      start scan
+        // if full scan has been complete (S = 1) -> switch to the other array
         //  note that data response packets will stop if HW failure is detected
-        //      eventually, add timeout and reset
 
         
-        int bytestoread = serialPort.getBytesReceived();
-        byte bytes[];
+        // int bytestoread = serialPort.getBytesReceived();
+        // byte bytes[];
 
-        if (bytestoread >= 9 ) {
-            bytes = serialPort.read(bytestoread);
+        // if (bytestoread >= 9 ) {
+        //     bytes = serialPort.read(bytestoread);
 
-            parseData(bytes);
-            //request a new scan
-            //System.out.println(Arrays.toString(bytes));
-            serialPort.write(startCommand, startCommand.length);
+        //     parseData(bytes);
+        //     //request a new scan
+        //     //System.out.println(Arrays.toString(bytes));
+        //     serialPort.write(startCommand, startCommand.length);
         }
-    }
 
     @Override
     public void initSendable(SendableBuilder builder) {
--- src/main/java/frc/robot/subsystems/Scan.java
+++ src/main/java/frc/robot/subsystems/Scan.java
@@ -5,13 +5,30 @@
 package frc.robot.subsystems;
 
 /** Add your docs here. */
-public class LidarData {
+public class Scan {
     //sampling mode once receives express scan request - get_lidar_conf (command to get typical scan mode)
     //quality, angle, angle, distance, distance
     //actual heading = angle_q6/64.0 degree
     //actual distance = distance_q2/4.0 mm
-    private float quality;
-    private float angle;
-    private float distance;
+    float range;
+    float angle;
+    float quality;
+    public Scan(float range, float angle, float quality){
+        this.range = range;
+        this.angle = angle;
+        this.quality = quality;
+    }
+
+    public float getRange(){
+        return range;
+    }
+    
+    public float getAngle(){
+        return angle;
+    }
+
+    public float getQuality(){
+        return quality;
+    }
 
 }
