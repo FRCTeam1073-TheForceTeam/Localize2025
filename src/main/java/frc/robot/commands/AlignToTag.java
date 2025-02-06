65f55343f05e10de309f878c1dce3f6ffc4c90d9
Arjun <92765324+Mindi088@users.noreply.github.com>
2025-02-05 20:46:08 -0500

    William, Arjun, Mindi align stuff

---
+++ src/main/java/frc/robot/commands/AlignToTag.java
@@ -0,0 +1,86 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Path.Point;
+import frc.robot.commands.Path.Segment;
+import frc.robot.subsystems.AprilTagFinder;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Localizer;
+import frc.robot.subsystems.FieldMap;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class AlignToTag extends Command {
+  Drivetrain drivetrain;
+  AprilTagFinder tagFinder;
+  Localizer localizer;
+  FieldMap fieldMap;
+
+  Map<Integer, Double> tagThetas = new HashMap<>();
+
+
+  /** Creates a new alignToTag. */
+  public AlignToTag(Drivetrain drivetrain, AprilTagFinder tagFinder, Localizer localizer, FieldMap fieldMap) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.drivet rain = drivetrain;
+    this.tagFinder = tagFinder;
+    this.localizer = localizer;
+    this.fieldMap = fieldMap;
+    tagThetas.put(11, Math.PI / 4);
+    tagThetas.put(10, 0.0);
+    tagThetas.put(9, - Math.PI / 4);
+    tagThetas.put(8, - 3 * Math.PI / 4);
+    tagThetas.put(7, Math.PI);
+    tagThetas.put(6, Math.PI / 4);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  public Command aprilTagAlign(AprilTag aprilTag, String side) {
+    Pose2d currentPose = localizer.getPose();
+    Optional<Pose3d> tagPose = FieldMap.fieldMap.getTagPose(aprilTag.ID);
+    if(!tagPose.isPresent()) {
+      // it doesn't have a pose to give us yet, bail
+      return null;
+    }
+    double theta = (tagThetas.get(aprilTag.ID) != null) ? tagThetas.get(aprilTag.ID) : 0;
+    Pose3d theActualPose = tagPose.get();
+    Point start = new Point(currentPose.getX(), currentPose.getY());
+    Point destination = new Point(theActualPose.getX() - 0.35 * Math.cos(theta), theActualPose.getY() - 0.35 * Math.sin(theta));
+
+    ArrayList<Segment> segment = new ArrayList<Segment>();
+    segment.add(new Segment(start, destination, theta, 0.5));
+
+    Path path = new Path(segment, theta);
+    return new SequentialCommandGroup(new DrivePath(drivetrain, path, localizer));
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/subsystems/FieldMap.java
+++ src/main/java/frc/robot/subsystems/FieldMap.java
@@ -3,8 +3,14 @@
 package frc.robot.subsystems;
 
 
+import java.util.List;
+import java.util.Optional;
+
+import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 
 
 public class FieldMap
