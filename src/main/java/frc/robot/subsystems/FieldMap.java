3678bf9080c0dea4626753ecd05331be6885f721
William <mindi.h@frc1073.org>
2025-02-05 19:51:52 -0500

    added getbestAprilTag

--- src/main/java/frc/robot/subsystems/FieldMap.java
+++ src/main/java/frc/robot/subsystems/FieldMap.java
@@ -11,4 +11,44 @@ public class FieldMap
 {
     public static final AprilTagFieldLayout fieldMap = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
 
+    private Localizer localizer;
+    private Pose2d robot2DPose = localizer.getPose();
+    //private ArrayList<Double> distances = new ArrayList<Double>();
+   
+    private List<AprilTag> aprilTags = fieldMap.getTags();
+    private AprilTag aprilTag;
+    
+    // public FieldMap()
+    // {
+
+    // }
+
+    public AprilTag getBestAprilTag(Pose2d robot2DPose, List<AprilTag> aprilTags){
+        double shortestDistance = 100;
+
+        for(AprilTag tag : aprilTags) {
+            if (findDistance(robot2DPose, tag.ID) < shortestDistance)
+                aprilTag = tag;
+        }
+
+        return aprilTag;
+    }
+
+    public double findDistance(Pose2d robot2DPose, int tagID){
+
+        Optional<Pose3d> tag3dPose = fieldMap.getTagPose(tagID);
+
+        if (!tag3dPose.isPresent())
+        {
+            //we don't have a value to work with, bail
+            return -1.0;
+        }
+
+        double tagX = tag3dPose.get().getX();
+        double tagY = tag3dPose.get().getY();
+
+        double distance = Math.sqrt(Math.pow((tagX - robot2DPose.getX()), 2) + Math.pow((tagY - robot2DPose.getY()), 2));
+        return distance;
+    }
+
 }
